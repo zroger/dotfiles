2cf6dbb294d7a9f78315bbe0ce7c8cb9f2968132
Roger López <lopez.roger@gmail.com>
2011-05-06 08:34:16 -0400

    adding .drush directory

---
+++ drush/energy.aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+$aliases['energy-local'] = array(
+  'uri' => 'energy.local',
+  'root' => '/Users/rogerlopez/treehouse/energy.gov/docroot',
+  'command-specific' => array(
+    'site-install' => array(
+      'profile' => 'energy',
+      'site-name' => 'Energy.gov (local)',
+    ),
+  ),
+);
