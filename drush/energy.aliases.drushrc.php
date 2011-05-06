d06fbef47b2f4610c12567d50824ac86aafb0736
Roger López <lopez.roger@gmail.com>
2011-05-06 10:45:08 -0400

    use shorter aliases in the energy group

--- drush/energy.aliases.drushrc.php
+++ drush/energy.aliases.drushrc.php
@@ -1,11 +1,10 @@
 <?php
 
-$aliases['energy-local'] = array(
+$aliases['local'] = array(
   'uri' => 'energy.local',
   'root' => '/Users/rogerlopez/treehouse/energy.gov/docroot',
   'command-specific' => array(
     'site-install' => array(
-      'profile' => 'energy',
       'site-name' => 'Energy.gov (local)',
     ),
   ),
