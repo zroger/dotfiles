c7ac4033e8f61c7e2549c80858c5ee7ddcbf21e1
Roger López <lopez.roger@gmail.com>
2011-12-11 20:16:55 -0500

    misc updates

--- drush/energy.aliases.drushrc.php
+++ drush/energy.aliases.drushrc.php
@@ -1,8 +1,8 @@
 <?php
 
 $aliases['local'] = array(
-  'uri' => 'energy.local',
-  'root' => '/Users/rogerlopez/treehouse/energy.gov/docroot',
+  'uri' => 'http://energy.local:3000/',
+  'root' => '/Users/zroger/treehouse/energy.gov/docroot',
   'command-specific' => array(
     'site-install' => array(
       'site-name' => 'Energy.gov (local)',
